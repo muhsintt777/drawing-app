c020ba549e1537c3625a6350a5a92ca118776d16
Muhsin TT <muhsintt777@gmail.com>
2022-12-14 00:10:51 +0530

    created canvas compo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Canvas from "./components/canvas/Canvas";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Canvas />
+    </div>
+  );
 }
 
 export default App;
