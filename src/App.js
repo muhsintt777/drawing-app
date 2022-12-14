8b80d8bea421c47714491858766ac0680668048f
Muhsin TT <muhsintt777@gmail.com>
2022-12-14 19:03:57 +0530

    changed state into ref

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import "./App.css";
 
 function App() {
   const canvasRef = useRef();
+  const point = useRef(0);
+  const ctx = useRef();
 
   useEffect(() => {
     window.addEventListener("mousemove", (e) => {
       console.log({ x: e.clientX, y: e.clientY });
-      const bondingRect = canvasRef.current.getBoundingClientRect();
-      console.log({
-        x: e.clientX - bondingRect.left,
-        y: e.clientY - bondingRect.top,
-      });
+      const boundingRect = canvasRef.current.getBoundingClientRect();
+      ctx.current = canvasRef.current.getContext("2d");
+      point.current = {
+        x: e.clientX - boundingRect.left,
+        y: e.clientY - boundingRect.top,
+      };
+      console.log(point.current);
     });
   }, []);
   return (
